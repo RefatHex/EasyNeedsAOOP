24931f00488cef5833de65235cd840b8aa587789
refat <khondakar00@gmail.com>
2023-10-10 11:09:08 -0700

    Added signin functionality and added consumer page ready to be functionalized.

---
+++ src/main/java/com/example/easyneedsaoop/ConsumerController.java
@@ -0,0 +1,36 @@
+package com.example.easyneedsaoop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.ImageView;
+
+public class ConsumerController {
+
+    @FXML
+    private ImageView DoctorIcon;
+
+    @FXML
+    private ImageView HospitalIcon;
+
+    @FXML
+    private ImageView clothingIcon;
+
+    @FXML
+    private ImageView eduationIcon;
+
+    @FXML
+    private ImageView foodIcon;
+
+    @FXML
+    private ImageView homeImage;
+
+    @FXML
+    private Hyperlink myAccountButton;
+
+    @FXML
+    private Hyperlink rewardsLink;
+
+    @FXML
+    private ImageView shoppingIcon;
+
+}
