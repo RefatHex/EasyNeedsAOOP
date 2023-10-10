cae000faedbfa1f9f54b9d95cb83c55ccea4ffa2
S.M. Nabiul Islam <nabilnabiulislam@gmail.com>
2023-10-10 23:31:58 +0600

    function added

--- src/main/java/com/example/easyneedsaoop/Main.java
+++ src/main/java/com/example/easyneedsaoop/Main.java
@@ -23,5 +23,8 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    public void hello(){
+        System.out.println("Hello");
+    }
 
 }
