6c4c919b28070c3c21ed5a476bbf4c5a642eeeb9
S.M. Nabiul Islam <nabilnabiulislam@gmail.com>
2023-10-10 23:40:05 +0600

    changes reverted

--- src/main/java/com/example/easyneedsaoop/Main.java
+++ src/main/java/com/example/easyneedsaoop/Main.java
@@ -23,8 +23,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    public void hello(){
-        System.out.println("Hello");
-    }
 
 }
