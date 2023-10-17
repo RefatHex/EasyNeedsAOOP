b111ca25ca0828e62a4b59c76c409de9f0c7816f
S.M. Nabiul Islam <nabilnabiulislam@gmail.com>
2023-10-18 00:59:18 +0600

    Created and Designed HomeOwner-Tenant Agreement and fixed some UI errors

--- src/main/java/com/example/easyneedsaoop/AdminControlPage.java
+++ src/main/java/com/example/easyneedsaoop/AdminControlPage.java
@@ -1,4 +1,16 @@
 package com.example.easyneedsaoop;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class AdminControlPage {
+    @FXML
+    private Button signoutButton;
+
+    @FXML
+    public void signoutButtonAction(){
+        Stage stage = (Stage)signoutButton.getScene().getWindow();
+        stage.close();
+    }
 }
---
+++ src/main/java/com/example/easyneedsaoop/OwnerTenantAgreementController.java
@@ -0,0 +1,13 @@
+package com.example.easyneedsaoop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class OwnerTenantAgreementController {
+    @FXML
+    private Button completeButton;
+    @FXML
+    public void completeButtonAction(){
+
+    }
+}
