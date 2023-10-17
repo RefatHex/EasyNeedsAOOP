c1edb6d1c4310e178d3200b923586586de0fb9b0
S.M. Nabiul Islam <nabilnabiulislam@gmail.com>
2023-10-18 02:10:34 +0600

    Modified colors, corrected some errors and added minor functionality

--- src/main/java/com/example/easyneedsaoop/AdminControlPage.java
+++ src/main/java/com/example/easyneedsaoop/AdminControlPage.java
@@ -1,15 +1,19 @@
 package com.example.easyneedsaoop;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminControlPage {
     @FXML
     private Button signoutButton;
 
     @FXML
-    public void signoutButtonAction(){
+    public void signoutButtonAction() throws IOException {
         Stage stage = (Stage)signoutButton.getScene().getWindow();
         stage.close();
     }
--- src/main/java/com/example/easyneedsaoop/OwnerTenantAgreementController.java
+++ src/main/java/com/example/easyneedsaoop/OwnerTenantAgreementController.java
@@ -1,6 +1,7 @@
 package com.example.easyneedsaoop;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 public class OwnerTenantAgreementController {
@@ -8,6 +9,11 @@ public class OwnerTenantAgreementController {
     private Button completeButton;
     @FXML
     public void completeButtonAction(){
-
+        Alert alert;
+        alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Massage");
+        alert.setHeaderText(null);
+        alert.setContentText("Terms & Conditions Successfully Submitted");
+        alert.showAndWait();
     }
 }
