705d1eef14405ed0c619546d528c68d39d6e36d0
S.M. Nabiul Islam <nabilnabiulislam@gmail.com>
2023-10-19 00:12:11 +0600

    Designed UserType fxml and Fixed some previous errors.

---
+++ src/main/java/com/example/easyneedsaoop/UserTypeController.java
@@ -0,0 +1,46 @@
+package com.example.easyneedsaoop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class UserTypeController {
+
+    @FXML
+    private Button firstBtn;
+    @FXML
+    private Button secondBtn;
+    @FXML
+    private Button thirdBtn;
+    @FXML
+    private Button fourthBtn;
+    @FXML
+    private Button fifthBtn;
+    @FXML
+    private Button sixthBtn;
+
+    @FXML
+    public void firstBtnAction(){
+
+    }
+    @FXML
+    public void secondBtnAction(){
+
+    }
+    @FXML
+    public void thirdBtnAction(){
+
+    }
+    @FXML
+    public void fourthBtnAction(){
+
+    }
+    @FXML
+    public void fifthBtnAction(){
+
+    }
+    @FXML
+    public void sixthBtnAction(){
+
+    }
+
+}
