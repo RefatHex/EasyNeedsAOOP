d798c0b4db3bbb2d86784383e3763a8c6f3e41a9
S.M. Nabiul Islam <nabilnabiulislam@gmail.com>
2023-11-26 01:53:15 +0600

    fixed NullPointerException errors and added actions

--- src/main/java/com/example/easyneedsaoop/Consumer_Food_Controller.java
+++ src/main/java/com/example/easyneedsaoop/Consumer_Food_Controller.java
@@ -1,4 +1,14 @@
 package com.example.easyneedsaoop;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
 public class Consumer_Food_Controller {
+
 }
--- src/main/java/com/example/easyneedsaoop/Main.java
+++ src/main/java/com/example/easyneedsaoop/Main.java
@@ -12,10 +12,10 @@ import java.io.IOException;
 
 public class Main extends Application {
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("cateringPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("loginSignup.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
-//        Image image = new Image("D:\\Study Materials\\Trimester-4\\AOOP\\EasyNeedsAOOP\\src\\main\\resources\\com\\example\\easyneedsaoop\\EasyNeed.png");
-//        ImageView imageView = new ImageView(image);
+        Image image = new Image("D:\\Study Materials\\Trimester-4\\AOOP\\EasyNeedsAOOP\\src\\main\\resources\\com\\example\\easyneedsaoop\\EasyNeed.png");
+        ImageView imageView = new ImageView(image);
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("EasyNeeds");
         stage.setScene(scene);
